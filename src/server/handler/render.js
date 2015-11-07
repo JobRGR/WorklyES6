25670249502693e96daafa7673efce11ec2c709d
A-gambit <a.gambit.gregory@gmail.com>
2015-11-07 14:57:29 +0200

    module

--- src/server/handler/render.js
+++ src/server/handler/render.js
@@ -1,5 +1,1 @@
-export default (req, res) => {
-  let i = 1
-  console.log(i)
-  res.render('index.html')
-}
+export default (req, res) => res.render('index.html')
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,22 @@
 import express from 'express'
 import render from '../handler/render'
+import University from '../handler/university'
 
 export default () => {
   let app = express()
-  app.get('/', render)
+
+  let api = express.Router()
+
+  let universityApi = express.Router()
+    .get('/', University.getAll)
+    .get('/:id', University.getItem)
+    .put('/:id', University.updateItem)
+    .delete('/', University.removeAll)
+    .delete('/:id', University.removeItem)
+
+  api.use('/university', universityApi)
+  app
+    .use('/api', api)
+    .get('/', render)
   return app
 }
