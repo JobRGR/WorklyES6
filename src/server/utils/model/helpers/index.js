b95d9c935233c061357e7c981ad462c97fee3e33
A-gambit <a.gambit.gregory@gmail.com>
2015-11-16 22:34:47 +0200

    refactor experience-education schema

--- src/server/utils/model/helpers/index.js
+++ src/server/utils/model/helpers/index.js
@@ -1,12 +1,12 @@
-import removeItem from './remove'
-import getCount from './count'
-import getRandom from './random'
-import getItem from './get'
-import updateItem from './update'
-import autocomplite from './autocomplite'
-import searchItem from './search'
-import randomTowPopulate from './random_tow_populate'
-import getTowPopulation from './get_tow_population'
+import removeItem from './lib/remove'
+import getCount from './lib/count'
+import getRandom from './lib/random'
+import getItem from './lib/get'
+import updateItem from './lib/update'
+import autocomplite from './lib/autocomplite'
+import searchItem from './lib/search'
+import randomTowPopulate from './lib/random_tow_populate'
+import getTowPopulation from './lib/get_tow_population'
 
 export default {
   removeItem,
