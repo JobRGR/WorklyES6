17df01cd04c17eba6d1b06785ab1bb50521f56c4
A-gambit <a.gambit.gregory@gmail.com>
2015-11-22 14:02:29 +0200

    session

--- src/server/middeware/student.js
+++ src/server/middeware/student.js
@@ -3,9 +3,8 @@ import Student from '../models/student'
 export default (req, res, next) => {
   req._student = res.locals._student = null
   if (!req.session._student) return next()
-  Student.findById(req.session._student, (err, student) => {
-    if (err) return next(err)
-    req._student = res.locals._student = student
-    next()
+  Student.getItem(req.session._student, (err, _student) => {
+    req._student = res.locals._student = _student
+    next(err)
   })
 }
