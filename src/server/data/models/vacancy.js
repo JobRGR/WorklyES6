bdc680507afe10c7afc4bbc879ecc38d77eee561
Gregory Shehet <a.gambit.gregory@gmail.com>
2016-04-10 23:39:59 +0300

    update generation

--- src/server/data/models/vacancy.js
+++ src/server/data/models/vacancy.js
@@ -1,6 +1,6 @@
 import Charlatan from 'charlatan'
 import async from 'async'
-import {City, Skill, Company, Vacancy} from '../../models/models'
+import {City, Skill, Company, Vacancy, Position} from '../../models/models'
 import addArray from '../utils/add_array'
 import random from '../utils/random'
 
@@ -8,13 +8,17 @@ let data = []
 
 for (let i = 0; i < 50; i++) {
   data.push({
-    name: Charlatan.Lorem.sentence(5),
+    name: '',
     about: Charlatan.Lorem.text(3, 20, '\n'),
     skills: []
   })
 }
 
 export default (cb) => async.waterfall([
+  callback => async.each(data, (item, next) => Position.getRandom((err, position) => {
+    item.name = position.name
+    next()
+  }), err => callback()),
   callback => async.each(data, (item, next) => City.getRandom((err, city) => {
     item.city = city._id
     next()
