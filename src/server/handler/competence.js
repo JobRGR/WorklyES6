523d8e21b6ce61a1a5766265fa51327bd06b16bf
Gregory Shehet <a.gambit.gregory@gmail.com>
2016-05-17 22:56:40 +0300

    update api

--- src/server/handler/competence.js
+++ src/server/handler/competence.js
@@ -1,17 +1,19 @@
 import wikipedia from 'node-wikipedia'
-import toMarkdown from 'html-md'
+import htmlToText from 'html-to-text'
 import searchImage from '../utils/search_image'
 
 export default {
   wikipedia(req, res, next) {
     wikipedia.page.data(req.query.value, {content: true}, data => {
       try {
-        res.text = data.text['*']
-        res.markdownText = toMarkdown(res.text)
+        res.html = data.text['*']
+        res.text = htmlToText.fromString(res.html, {
+          wordwrap: 130
+        })
       }
       catch (err) {
+        res.html = null
         res.text = null
-        res.markdownText = null
       }
       finally {
         next()
@@ -20,16 +22,16 @@ export default {
   },
 
   duckduckgo(req, res, next) {
-    searchImage(req.query.value, image => {
+    searchImage(req.query.value + '%20logo', image => {
       res.image = image
       next()
     })
   },
 
   send(req, res) {
-    let {image, text, markdownText} = res
+    let {image, html, text} = res
     let {value: title} = req.query
-    let competence = {image, title, text, markdownText}
+    let competence = {image, title, text, html}
     res.send({competence})
   }
 }
