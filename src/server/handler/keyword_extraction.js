fb68e637e2024b24f74bc70954de6ee2536a96c0
Ignat <IOBveritas@users.noreply.github.com>
2017-06-18 21:21:42 +0300

    fix null behaviour

--- src/server/handler/keyword_extraction.js
+++ src/server/handler/keyword_extraction.js
@@ -5,6 +5,9 @@ import {Vacancy} from '../models/models'
 export default {
   extract(req, res, next) {
     const {text} = req.body
+    if (!text || !text.trim() || text.search(/^\s+$/ig) != -1) {
+      return res.send({list: []})
+    }
     Vacancy.getAll((data) => {
       const corpus = data.reduce((res, item) => {
         return [...res, item.about]
